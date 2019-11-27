a5b658150e882eea5a167c880db5c49f1b1e90a6
Billy_Bat <gguillaume.gonde@gmail.com>
2019-11-27 15:26:27 +0900

    One hot encoding added + fixes

--- lib/utils.py
+++ lib/utils.py
@@ -1,6 +1,7 @@
 
 from sklearn import preprocessing
 import pandas as pd
+import numpy as np
 
 
 
@@ -10,3 +11,13 @@ def pd_normalize(Data) :
     x_n = scaler.fit_transform(x)
     data_n = pd.DataFrame(x_n, index=Data.index, columns=Data.columns)
     return data_n, scaler
+
+def pd_one_hot_encoder(Data) :
+    cols = Data.columns
+    Ohe_df = pd.DataFrame(index=Data.index, columns=None)
+    for col in Data.columns :
+        one_hot = pd.get_dummies(Data[col])
+        # Join the encoded df
+        Ohe_df = pd.concat((Ohe_df, one_hot), axis='columns')
+
+    return Ohe_df
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from lib.pd_load_data import *
 from lib.analysis import *
 from lib.utils import *
+from Network_setup import *
 import matplotlib.pyplot as plt
 
 
@@ -14,7 +15,15 @@ if __name__ == '__main__' :
     Data.drop(features_nocorr, axis='columns', inplace=True)
 
     # Normalize continuous values
-    continuous_col = Data.select_dtypes(include=(np.int64, int, float))
-    continuous_n, scaler = pd_normalize(continuous_col)
+    continuous_df = Data.select_dtypes(include=(np.int64, int, float))
+    continuous_n, scaler = pd_normalize(continuous_df)
     for col in continuous_n.columns :
         Data[col] = continuous_n[col]
+
+    # Deal With Caterorical Data (One Hot encoding)
+    categorical_df = Data.select_dtypes(include=(object))
+    Ohe_df = pd_one_hot_encoder(categorical_df)
+    Data.drop(columns=categorical_df.columns, inplace=True)
+    Raw_input = pd.concat((Data, Ohe_df), axis='columns')
+
+    # Create the Network that best suits the data
